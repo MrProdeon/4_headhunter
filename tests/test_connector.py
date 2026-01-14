3ddbce333db0d0be05d91c70f02ce2de02e9317d
Prodeon <prodeon21@gmail.com>
2026-01-14 17:50:32 +0300

    Тест исправлен из-за изменения функционала

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -11,20 +11,20 @@ params = {"text": "", "page": 0, "per_page": 100}
 
 @patch("requests.get")
 def test_headhunter_api(mock_get):
-    mock_get.return_value.json.return_value = {"items": {"id": 1, "name": "python-developer", "salary": 90000}}
+    mock_get.return_value.json.return_value = {"items": [{"id": 1, "name": "python-developer", "salary": 90000}]}
 
-    hh_api = HeadHunterApi("https://api.hh.ru/vacancies", {"User-Agent": "test for skypro"})
+    hh_api = HeadHunterApi({"User-Agent": "test for skypro"})
 
-    assert hh_api._connect(params) == {"items": {"id": 1, "name": "python-developer", "salary": 90000}}
-    assert hh_api.get_vacancies() == {"id": 1, "name": "python-developer", "salary": 90000}
+    assert hh_api._connect(params, url="https://api.hh.ru/vacancies") == {"items": [{"id": 1, "name": "python-developer", "salary": 90000}]}
+    assert hh_api.get_vacancies() == [{"id": 1, "name": "python-developer", "salary": 90000}]
 
 
 @patch("requests.get")
 def test_headhunter_api_error(mock_get):
     mock_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError()
-    hh_api = HeadHunterApi("https://api.hh.ru/vacancies", {"User-Agent": "test for skypro"})
-    try_connect = hh_api._connect(params)
+    hh_api = HeadHunterApi({"User-Agent": "test for skypro"})
+    try_connect = hh_api._connect(params,url="https://api.hh.ru/vacancies")
 
-    assert try_connect == {}
+    assert try_connect == None
 
     hh_api.get_vacancies()
