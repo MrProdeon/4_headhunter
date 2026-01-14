e9e326fb12e396b599f7e8da7876a777382fe69a
Prodeon <prodeon21@gmail.com>
2026-01-14 18:36:43 +0300

    Описана точка входа для работы программы

--- main.py
+++ main.py
@@ -1,123 +1,36 @@
 from src.connector import HeadHunterApi
 from src.FileWorker import JsonWorker
 from src.Vacancy import Vacancy
-from utils.functions import dicts_to_objects, end_or_continue, print_vacancies, ranged_vacancies, top_n
+from utils.functions import dicts_to_objects, end_or_continue, print_vacancies, ranged_vacancies, top_n, only_vacancies
+from utils.database_functions import database_vacancies_and_employers
 
 hh_api = HeadHunterApi({"User-Agent": "test for skypro"})
 json_worker = JsonWorker("data/json_data.json")
 
 
-def main() -> None:
+def main():
     while True:
-        try:
-            target = sorted(
-                list(
-                    map(
-                        int,
-                        input(
-                            """По каким критериям отфильтровать вакансии?
-1 - ключевые слова
-2 - по диапазону зарплат
-3 - по самым высоким зарплатам
-4 - удалёнка или офис
-5 - просмотреть уже записанные вакансии из файла (если этот пункт будет выбран, то будет обработан только он)
-(Напишите в одну строку через пробел)
-"""
-                        )
-                        .strip()
-                        .split(),
-                    )
-                )
+        user_choise = ""
+        while user_choise not in ("1", "2"):
+            user_choise = input(
+                """Каким сервисом хотите воспользоваться?
+1 - Поиск вакансий с возможностью выбора критериев для поиска и записью информации в файл или консоль
+2 - Поиск вакансий 10 топовых работодателей с записью в базу данных и получение данных из неё
+Ваш выбор: """
             )
-        except ValueError:
+
+        if user_choise == "1":
+            only_vacancies()
+        elif user_choise == "2":
+            database_vacancies_and_employers()
+
+        is_work_end = ""
+        while is_work_end not in ("1", "2"):
+            is_work_end = input("Хотите воспользоваться другим сервисом? 1 - Да, 2 - Нет.")
+
+        if is_work_end == "1":
             continue
-        if all(number in (1, 2, 3, 4, 5) for number in target):
-
-            if 5 in target:
-                vacancies = json_worker.get_data()
-                if len(vacancies) == 0:
-                    print("Пусто")
-                    if end_or_continue():
-                        continue
-                    break
-                else:
-                    vacancies_objects = dicts_to_objects(vacancies)
-                    print_vacancies(vacancies_objects)
-                    if end_or_continue():
-                        continue
-                    break
-
-            if 1 in target:
-                name = input("Введите ключевые слова: ")
-                vacancies = hh_api.get_vacancies(name)
-            else:
-                vacancies = hh_api.get_vacancies()
-
-            if 4 in target:
-                work_f = input("1 - для поиска удалёнки, 2 - для поиска офиса, 3 - гибрид")
-                while work_f not in ("1", "2", "3"):
-                    work_f = input("1 - для поиска удалёнки, 2 - для поиска офиса, 3 - гибрид")
-                if work_f == "1":
-                    vacancies = [
-                        vacancy
-                        for vacancy in vacancies
-                        if any(work_format["id"] == "REMOTE" for work_format in vacancy["work_format"])
-                    ]
-                if work_f == "2":
-                    vacancies = [
-                        vacancy
-                        for vacancy in vacancies
-                        if any(work_format["id"] == "ON_SITE" for work_format in vacancy["work_format"])
-                    ]
-                if work_f == "3":
-                    vacancies = [
-                        vacancy
-                        for vacancy in vacancies
-                        if any(work_format["id"] == "HYBRID" for work_format in vacancy["work_format"])
-                    ]
-
-            vacancies = Vacancy.cast_to_object_list(vacancies)
-
-            if 2 in target:
-                while True:
-                    range_from_str = input("Введите начальную зарплату: ")
-                    range_to_str = input("Введите конечную зарплату: ")
-                    try:
-                        range_from = int(range_from_str)
-                        range_to = int(range_to_str)
-                        break
-                    except ValueError:
-                        print("Введите число!")
-                        continue
-
-                vacancies = ranged_vacancies(vacancies, range_from, range_to)
-
-            if 3 in target:
-                n_str = input("Какое отобразить количество вакансий с максимальной ЗП?  ")
-                while True:
-                    try:
-                        n = int(n_str)
-                        break
-                    except ValueError:
-                        print("Введите число!")
-                        continue
-                vacancies = top_n(vacancies, n)
-
-            to_file = input("Хотите записать вакансии в файл? 1 - да, 2 - нет : ")
-            while to_file not in ("1", "2"):
-                to_file = input("Хотите записать вакансии в файл? 1 - да, 2 - нет : ")
-            if to_file == "1":
-                for vacancy in vacancies:
-                    json_worker.add_data(vacancy)
-
-            print_vacancies(vacancies)
-
-            if end_or_continue():
-                continue
-            break
-
-
-# Сделать перелистывание страницы
+        break
 
 
 if __name__ == "__main__":
