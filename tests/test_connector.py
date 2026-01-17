dbadb95c22f9bf484b264e1a5df1f65899df0527
Prodeon <prodeon21@gmail.com>
2026-01-17 14:10:55 +0300

    Написаны тесты на функционал проекта

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -27,6 +27,20 @@ def test_headhunter_api_error(mock_get):
     hh_api = HeadHunterApi({"User-Agent": "test for skypro"})
     try_connect = hh_api._connect(params, url="https://api.hh.ru/vacancies")
 
-    assert try_connect == None
+    assert try_connect is None
 
     hh_api.get_vacancies()
+
+@patch("requests.get")
+def test_get_employers(mock_get):
+    mock_get.return_value.json.return_value = {"items" : ["test"]}
+    mock_get.status_code = 200
+
+    hh = HeadHunterApi({"User-Agent": "test for skypro"})
+
+    assert hh.get_employers(["test", "test2"]) == [["test"], ["test"]]
+
+def test_get_ids():
+    hh = HeadHunterApi({"User-Agent": "test for skypro"})
+    result = hh.get_ids([[{"id" : 1, "name" : "test1"}], [{"id" : 2, "name" : "test2"}]])
+    assert result == [{1: 'test1'}, {2: 'test2'}]
